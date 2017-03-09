def5b966aad412f820940ae9b0b2872a6b89974f
Vadim Pribylov <vpribylov@lemz.ru>
2017-03-09 21:00:51 +0300

    added NMLNetwork class for multi-layer

--- ann_draft.py
+++ ann_draft.py
@@ -83,7 +83,7 @@ class NNeuron():
         Creates a neuron with relu activation function
 
         Args:
-            inputs: `int`. Amount of inputs of neuron
+            inputs: `list of Tensor`. Tensors that we connect to the neuron
             type: `str='relu` or any other `str`. type of activation function
             name: `str`. Name of tensor node.
 
@@ -127,8 +127,8 @@ class NLayer():
         given number of inputs plus one (for a constant input)
 
         Args:
-            inputs: `int`. Number of inputs for each neuron to have.
-            num_neurons: `int`. Number of neurons inside layer.
+            inputs: `list of Tensor` inputs connected to the layer
+            num_neurons: `int`. Number of neurons inside layer
             name: `str`. Name scope of layer
 
         Returns:
@@ -160,6 +160,48 @@ class NLayer():
     def get_out(self):
         """returns output of layer"""
         return [neur.oo for neur in self.VN]
+
+
+class NMLNetwork():
+    def __init__(self, inputs, layout_list, name=None):
+        """Neural multi-layer network --- creates multi-layered network
+
+        Creates network structure according to `layout_list`
+
+        Args:
+            inputs: `Tensors` connected to the first layer
+            layout_list: `iterable`. Preferably list of `int` of neurons in
+            each layer accordingly. Total layer count equal to len(layout_list)
+            name: `str`. name scope of MLN
+
+        Returns:
+            object of NMLNetwork class
+
+        Raises:
+            TypeError if inputs or layout_list are not iterable
+        """
+        try:
+            iter(inputs)
+            iter(layout_list)
+        except TypeError:
+            raise TypeError('inputs/layout_list must be iterable')
+        self.default_name = 'multi-layer'
+        if not(name):
+            self.name = self.default_name
+        else:
+            self.name = name
+        with tf.name_scope(self.name):
+            self.LL = [0]*len(layout_list)
+            lcount = 1
+            self.LL[0] = NLayer(inputs, layout_list[0], name='layer0')
+            # probably weird behaviour if `layout_list`
+            # is short (less than 2 items)
+            for num_neurons in layout_list[1:]:
+                self.LL[lcount] = NLayer(
+                        self.LL[lcount - 1].get_out(),
+                        num_neurons, name='layer{}'.formar(lcount))
+                lcount += 1
+
 
 
 def form_feeder(feed_who, feed_what, pick):
